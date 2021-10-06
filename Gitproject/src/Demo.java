e62a80d3b65fb8fed4e545ec20e4b57eced7086e
vaibhav649 <vaibhav.j.raut@gmail.com>
2021-10-06 14:48:58 -0500

    by pritam

--- Gitproject/src/Demo.java
+++ Gitproject/src/Demo.java
@@ -1,5 +1,11 @@
 
 public class Demo {
+	
+	
+	public void m1()
+	{
+		System.out.println("this is m1");
+	}
 	public static void main(String[] args) {
 		System.out.println("this is git project");
 	}
