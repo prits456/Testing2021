cfba2a427f66f6e175392632a502542a1fb5ef2c
vaibhav649 <vaibhav.j.raut@gmail.com>
2021-10-10 13:13:44 -0500

    bu p

--- Gitproject/src/Demo1.java
+++ Gitproject/src/Demo1.java
@@ -3,7 +3,10 @@ public class Demo1 {
 public static void main(String[] args) {
 	System.out.println("demo1.........");
 	Demo1 d=new Demo1();
-	S
-}
 	
+}
+	public void m1()
+	{
+		System.out.println("this is m1");
+	}
 }
